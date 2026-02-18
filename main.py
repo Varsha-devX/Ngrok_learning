59810fbc267a4939c1b3d7e3f2fb03558a0f803a
Varsha <varsh6362@gmail.com>
2026-02-18 09:58:08 +0530

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    fastapi.run(app, host="[IP_ADDRESS]", port=8000)    
